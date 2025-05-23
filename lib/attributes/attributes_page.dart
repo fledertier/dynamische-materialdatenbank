6d9e26958ae97c0c20474dcc2e6adbf2de24f7a2
Leon <leon.martin@stud.hs-hannover.de>
2025-05-23 19:58:11 +0200

    Import Attributes

--- lib/attributes/attributes_page.dart
+++ lib/attributes/attributes_page.dart
@@ -7,6 +7,7 @@ import '../header/header.dart';
 import '../widgets/sheet.dart';
 import 'attribute_details.dart';
 import 'attributes_export_button.dart';
+import 'attributes_import_button.dart';
 import 'attributes_list.dart';
 
 class AttributesPage extends StatefulWidget {
@@ -22,7 +23,9 @@ class _AttributesPageState extends State<AttributesPage> {
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
-      header: Header(actions: [AttributesExportButton()]),
+      header: Header(
+        actions: [AttributesImportButton(), AttributesExportButton()],
+      ),
       navigation: Navigation(page: Pages.attributes),
       body: Row(
         spacing: 24,
