da53a55c1aceb7858a99ad8f62d64bd05140e5eb
Luca <luca.martin@stud.hs-hannover.de>
2025-07-21 21:11:24 +0200

    Include color and material data into export

--- lib/attributes/attributes_export_button.dart
+++ lib/attributes/attributes_export_button.dart
@@ -1,3 +1,4 @@
+import 'package:dynamische_materialdatenbank/attributes/attribute_converter.dart';
 import 'package:dynamische_materialdatenbank/constants.dart';
 import 'package:dynamische_materialdatenbank/firestore_provider.dart';
 import 'package:dynamische_materialdatenbank/utils/web_utils.dart';
@@ -13,17 +14,48 @@ class AttributesExportButton extends ConsumerWidget {
       icon: const Icon(Icons.download_outlined),
       label: Text('Export'),
       onPressed: () async {
-        final attributes =
-            (await ref
-                    .read(firestoreProvider)
-                    .collection(Collections.attributes)
-                    .doc(Docs.attributes)
-                    .get())
-                .data();
+        final attributes = await readAttributes(ref);
         if (attributes != null) {
           downloadJson(attributes, 'attributes.json');
         }
+
+        final materials = await readMaterials(ref);
+        if (materials.isNotEmpty) {
+          downloadJson(materials.toList(), 'materials.json');
+        }
+
+        final colors = await readColors(ref);
+        if (colors != null) {
+          downloadJson(colors, 'colors.json');
+        }
       },
     );
+  }
+
+  Future<Json?> readAttributes(WidgetRef ref) async {
+    return (await ref
+            .read(firestoreProvider)
+            .collection(Collections.attributes)
+            .doc(Docs.attributes)
+            .get())
+        .data();
+  }
+
+  Future<Iterable<Json>> readMaterials(WidgetRef ref) async {
+    return (await ref
+            .read(firestoreProvider)
+            .collection(Collections.materials)
+            .get())
+        .docs
+        .map((doc) => doc.data());
+  }
+
+  Future<Json?> readColors(WidgetRef ref) async {
+    return (await ref
+            .read(firestoreProvider)
+            .collection(Collections.colors)
+            .doc(Docs.materials)
+            .get())
+        .data();
   }
 }
